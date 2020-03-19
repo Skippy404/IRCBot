8fa84631312d71b893e1729f5d9d870dfb020e50
Tristan B. Kildaire <deavmi@localhost.localdomain>
2020-03-19 21:35:21 +0200

    Added thing

--- src/bot.py
+++ src/bot.py
@@ -1,1 +1,16 @@
-def startUp():
+import irc
+import irc.client
+
+def startUp(serverIP, serverPort, channelList):
+    # Create a client
+    client = irc.client.Reactor()
+
+    # Create a server connection
+    server = client.server()
+
+    # Connect to serverIP and serverPort (TODO: Loop through channel list)
+    server.connect(serverIP, serverPort, "#club45")
+    
+    pass
+
+startUp()
